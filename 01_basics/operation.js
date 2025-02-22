9269d3f65b1328a7486d939a2e6640d143521382
swaraj2003 <96387785+swaraj2003@users.noreply.github.com>
2025-02-22 04:25:01 +0000

    operation done

--- 01_basics/operation.js
+++ 01_basics/operation.js
@@ -1,4 +1,26 @@
-let score = 33
-
+let score = "33"
 console.log(typeof score);
 console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber);
+
+//"33" ==> 33
+//"33abc" ==> NAN
+// true ==> 1; false ==>0
+
+
+let isLogedIn = 1
+let booleanisLogedIn = Boolean(isLogedIn)
+console.log(booleanisLogedIn)
+
+// 1==> true or 0 ==> false
+//"" ==> false
+//"swaraj ==> true
+
+let Somenumber = 23
+let stringNumber = String(Somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
